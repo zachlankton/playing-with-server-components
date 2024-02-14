d584e86eb935386780c127fbfc247b8113a665bd
Zach Lankton <zachlankton@gmail.com>
2024-02-14 09:12:11 -0500

    add force dynamic

--- nextjs/src/app/page.tsx
+++ nextjs/src/app/page.tsx
@@ -1,7 +1,6 @@
 import * as fs from "fs";
 
-export default function Home({ searchParams }: { searchParams: any }) {
-  console.log(searchParams.asdf);
+export default function Home() {
   const val = fs.readFileSync("/tmp/test.txt", "utf-8");
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
@@ -10,3 +9,5 @@ export default function Home({ searchParams }: { searchParams: any }) {
     </main>
   );
 }
+
+export const dynamic = "force-dynamic";
