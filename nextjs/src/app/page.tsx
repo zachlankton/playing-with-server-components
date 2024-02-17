80c90bc463bf82d86f0bbb530c723c2d6b13d883
Zach Lankton <zachlankton@gmail.com>
2024-02-17 15:15:55 -0500

    remove counter from index in next example

--- nextjs/src/app/page.tsx
+++ nextjs/src/app/page.tsx
@@ -1,4 +1,4 @@
-import { Counter } from "@/components/CounterComponent";
+// import { Counter } from "@/components/CounterComponent";
 import { FileComponent } from "@/components/FileComponent";
 import Link from "next/link";
 
@@ -7,7 +7,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center p-24">
       <h1>Hello World</h1>
       <FileComponent path="/tmp/nextHome.txt" />
-      <Counter path="/tmp/nextHome.txt" />
+      {/* <Counter path="/tmp/nextHome.txt" /> */}
       <Link href="/about">Goto About</Link>
     </main>
   );
