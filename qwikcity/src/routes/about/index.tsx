da6cc33ccad0099b888924ff32fa13e832ac9668
Zach Lankton <zachlankton@gmail.com>
2024-02-18 12:54:20 -0500

    Qwik City Has Server Components?

--- qwikcity/src/routes/about/index.tsx
+++ qwikcity/src/routes/about/index.tsx
@@ -4,9 +4,9 @@ import FileComponent from "~/components/FileComponent";
 export default component$(() => {
   return (
     <>
-      <h1>Home</h1>
-      <FileComponent path="/tmp/qwikHome.txt" />
-      <a href="/about">Goto About</a>
+      <h1>About</h1>
+      <FileComponent path="/tmp/qwikAbout.txt" />
+      <a href="/">Goto Home</a>
     </>
   );
 });
